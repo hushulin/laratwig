f4236719123e095c303c93c70c5e398358e92496
胡树林 <hushulin12321@126.com>
2015-05-23 11:29:52 +0800

    first commit

---
+++ src/Hushulin/Laratwig/LaratwigServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Hushulin\Laratwig;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaratwigServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('hushulin/laratwig');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
